c4cdab9598eb7c6e45880d05c87b61a255a56af4
Balvikas <pscdt009@gmail.com>
2026-08-29 18:13:20 -0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// 🌍 GLOBAL CONFIGURATION MATRIX FOR BAL VIKAS ATTENDANCE
+// Update this URL ONCE here after a new deployment, and both pages update instantly!
+const GLOBAL_CONFIG = {
+    GOOGLE_SCRIPT_URL: "https://google.com"
+};
